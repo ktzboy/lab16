8555ffdff919a90cb47eb2510e343ca27c2ffdf7
ktzboy <pongpol.sritalanut@gmail.com>
2025-01-31 16:26:02 +0700

    finish

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,31 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *arr, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(arr + i);
+        if((i+1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *arr, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        arr[i] = rand()%101 / 100.0;
+    }
+}
+
+void findRowSum(const double *arr,double *sum,int N,int M) {
+    for(int i=0 ; i<N ; i++) {
+        for(int j=0 ; j<M ; j++) sum[i] += *(arr+(i*M)+j);
+    }
+}
+
+void findColSum(const double *arr,double *sum,int N,int M) {
+    for(int i=0 ; i<M ; i++) {
+        sum[i] = 0;
+        for(int j=0 ; j<N ; j++) sum[i] += *(arr+i+(j*M));
+    }
+}
